dd7679e5443b2adb7b5934cb1d48dc81fb457cb1
Dylan Peua <dylanpeua2@gmail.com>
2026-02-18 12:59:26 +0100

    API consultation disponibilités (Client)

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,9 @@
 const authRoutes = require('./auth.routes');
 const availabilityRoutes = require('./availability.routes');
+const publicRoutes = require('./public.routes');
 
 module.exports = {
   authRoutes,
   availabilityRoutes,
+  publicRoutes,
 };
